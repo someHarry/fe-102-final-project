af630b43518b095d9d5cf7bd9bd3c32cdd0fad90
someHarry <21parit@gmail.com>
2023-07-07 00:51:14 +0300

    add react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <p>Links:</p>
+                    <a href="/">Home page</a>
+                    <a href="/shop">Shop page</a>
+                    <a href="/shop/42">Item id 42 page</a>
+                    <a href="/cart">Cart page</a>
+                    <a href="/404">404 page</a>
+                </header>
+                <Routes>
+                    <Route path="/">
+                        <Route index element={
+                            <h2>Home page</h2>
+                        }
+                        />
+                        <Route path={"shop"} element={
+                            <h2>Shop page</h2>
+                        }
+                        />
+                        <Route path={"shop/*"} element={
+                            <h2>Item id {window.location.href.substr(window.location.href.indexOf("/shop/") + 6)} page</h2>
+                        }
+                        />
+                        <Route path={"cart"} element={
+                            <h2>Cart page</h2>
+                        }
+                        />
+                        <Route path={"*" || "404"} element={
+                            <h2>404 page</h2>
+                        }
+                        />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
