c8a8a6925743e014c68e4eb307f6a25af916ab28
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2023-07-09 21:40:30 +0300

    create branch

---
+++ client/src/Components/Button/Input/Input.js
@@ -0,0 +1,16 @@
+import { ReactPropTypes } from "react";
+import "./Input.scss";
+
+function Input(props) {
+  
+}
+
+Input.defaultProps = {
+  
+};
+
+Input.propTypes = {
+ 
+};
+
+export default Input;
