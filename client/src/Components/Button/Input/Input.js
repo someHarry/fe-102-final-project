e7411e28851631c4ff0262fc55bebc17b5933181
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2023-07-10 11:36:50 +0300

    add

--- client/src/Components/Button/Input/Input.js
+++ client/src/Components/Button/Input/Input.js
@@ -2,9 +2,19 @@ import { ReactPropTypes } from "react";
 import "./Input.scss";
 
 function Input(props) {
-  
-}
+  const { type, name, placeholder, label, className, error, errorMessage, ...inputProps } = props;
 
+  const classNames = className ? `form-item ${className}` : "form-item";
+  const hasValidation = error ? " has-validation" : "";
+
+  return (
+    <label className={classNames + hasValidation}>
+      <p className="form-label">{label}</p>
+      <input className="form-control" type={type} name={name} placeholder={placeholder} {...inputProps} />
+      {error && <p className="error-message">{errorMessage}</p>}
+    </label>
+  );
+}
 Input.defaultProps = {
   
 };
