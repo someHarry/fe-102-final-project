1c5c6a2d767c1e47886e700c1e308135c6172344
someHarry <21parit@gmail.com>
2023-07-11 20:34:14 +0300

    Add Product List

--- client/src/Components/ProductList/ProductList.jsx
+++ client/src/Components/ProductList/ProductList.jsx
@@ -5,22 +5,32 @@ import ProductCard from "../ProductCard";
 function ProductList({items}) {
 
     return (
-        <ul>
-            {items.map((item) => (
-                    <ProductCard
-                        id={item.id}
-                        name={item.name}
-                        variety={item.variety}
-                        region={item.region}
-                        country={item.country}
-                        image={item.image}
-                        price={item.price}
-                        discount={item.discount}
-                        key={item.id}
-                    />
-                )
-            )}
-        </ul>
+        <div className="product-list__wrapper">
+            <section className="product-list">
+                <div className="product-list__filter-block">
+                    Here will be filter panel component
+                </div>
+                <div className="product-list__content-block">
+                    <div className="product-list__sorting">Here will be sorting component</div>
+                    <ul className="product-list__products">
+                        {items.map((item) => (
+                                <ProductCard
+                                    id={item.id}
+                                    name={item.name}
+                                    variety={item.variety}
+                                    region={item.region}
+                                    country={item.country}
+                                    image={item.image}
+                                    price={item.price}
+                                    discount={item.discount}
+                                    key={item.id}
+                                />
+                            )
+                        )}
+                    </ul>
+                </div>
+            </section>
+        </div>
     )
 }
 
