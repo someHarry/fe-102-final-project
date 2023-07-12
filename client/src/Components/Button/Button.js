21cd58a62275708c75b8a220a3ebab384802c6d7
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2023-07-12 20:47:21 +0300

    form

--- client/src/Components/Button/Button.js
+++ client/src/Components/Button/Button.js
@@ -1,29 +1,29 @@
-// import { ReactPropTypes } from "react";
-// import "./Button.scss";
+import { ReactPropTypes } from "react";
+import "./Button.scss";
 
-// function Button(props) {
-//   return (
-//     <button
-//       className={"button " + this.props.btnStyles}
-//       type="button"
-//       onClick={this.props.btnClick}
-//     >
-//       {this.props.text}
-//     </button>
-//   );
-// }
+function Button(props) {
+  return (
+    <button
+      className={"button " + this.props.btnStyles}
+      type="button"
+      onClick={this.props.btnClick}
+    >
+      {this.props.text}
+    </button>
+  );
+}
 
-// Button.defaultProps = {
-//   type: "button",
-//   btnClick: () => {},
-//   btnStyles: "btn__bad",
-// };
+Button.defaultProps = {
+  type: "button",
+  btnClick: () => {},
+  btnStyles: "btn__bad",
+};
 
-// Button.propTypes = {
-//   btnStyles: PropTypes.string,
-//   type: PropTypes.string,
-//   btnClick: PropTypes.func,
-//   text: PropTypes.string,
-// };
+Button.propTypes = {
+  btnStyles: PropTypes.string,
+  type: PropTypes.string,
+  btnClick: PropTypes.func,
+  text: PropTypes.string,
+};
 
-// export default Button;
+export default Button;
