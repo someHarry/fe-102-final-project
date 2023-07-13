7119beb8d01ae4e5027643d75263c82093b516db
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2023-07-13 12:44:53 +0300

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,114 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import logo from './logo.svg'
-import Input from "./Components/Input/Input"
 import './App.css'
+import Header from './Components/Header/Header'
+import ProductList from './Components/ProductList'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <header className="App-header">
-          <Input/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          <p>Links:</p>
-          <a href="/">Home page</a>
-          <a href="/shop">Shop page</a>
-          <a href="/shop/42">Item id 42 page</a>
-          <a href="/cart">Cart page</a>
-          <a href="/404">404 page</a>
-        </header>
+        <Header />
+        {/* END HEADER */}
         <Routes>
           <Route path="/">
             <Route index element={<h2>Home page</h2>} />
-            <Route path="shop" element={<h2>Shop page</h2>} />
+            <Route
+              path="shop"
+              element={
+                <ProductList
+                  items={[
+                    {
+                      id: 1,
+                      name: 'Silver Oak Napa Valley Cabernet Sauvignon 2018',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Alexander Valley, Sonoma County',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_768,h_1059,c_fit,q_auto:good,fl_progressive/qcng8hwtfumjbahz9po6.jpg',
+                      price: 89.99,
+                      discount: 0,
+                    },
+                    {
+                      id: 2,
+                      name: 'The Prisoner Wine Company Cabernet Sauvignon 2019',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Napa Valley',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_480,h_600,c_fit,q_auto:good,fl_progressive/zluhfgt9fapsjfqlct4t.jpg',
+                      price: 61.99,
+                      discount: 0.2,
+                    },
+                    {
+                      id: 3,
+                      name: 'Silver Oak Napa Valley Cabernet Sauvignon 2018',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Alexander Valley, Sonoma County',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_768,h_1059,c_fit,q_auto:good,fl_progressive/qcng8hwtfumjbahz9po6.jpg',
+                      price: 89.99,
+                      discount: 0,
+                    },
+                    {
+                      id: 4,
+                      name: 'The Prisoner Wine Company Cabernet Sauvignon 2019',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Napa Valley',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_480,h_600,c_fit,q_auto:good,fl_progressive/zluhfgt9fapsjfqlct4t.jpg',
+                      price: 61.99,
+                      discount: 0.2,
+                    },
+                    {
+                      id: 5,
+                      name: 'Silver Oak Napa Valley Cabernet Sauvignon 2018',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Alexander Valley, Sonoma County',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_768,h_1059,c_fit,q_auto:good,fl_progressive/qcng8hwtfumjbahz9po6.jpg',
+                      price: 89.99,
+                      discount: 0,
+                    },
+                    {
+                      id: 6,
+                      name: 'The Prisoner Wine Company Cabernet Sauvignon 2019',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Napa Valley',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_480,h_600,c_fit,q_auto:good,fl_progressive/zluhfgt9fapsjfqlct4t.jpg',
+                      price: 61.99,
+                      discount: 0.2,
+                    },
+                    {
+                      id: 7,
+                      name: 'Silver Oak Napa Valley Cabernet Sauvignon 2018',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Alexander Valley, Sonoma County',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_768,h_1059,c_fit,q_auto:good,fl_progressive/qcng8hwtfumjbahz9po6.jpg',
+                      price: 89.99,
+                      discount: 0,
+                    },
+                    {
+                      id: 8,
+                      name: 'The Prisoner Wine Company Cabernet Sauvignon 2019',
+                      variety: 'Cabernet Sauvigon',
+                      region: 'Napa Valley',
+                      country: 'California',
+                      image:
+                        'https://www.wine.com/product/images/w_480,h_600,c_fit,q_auto:good,fl_progressive/zluhfgt9fapsjfqlct4t.jpg',
+                      price: 61.99,
+                      discount: 0.2,
+                    },
+                  ]}
+                />
+              }
+            />
             <Route
               path={'shop/*'}
               element={<h2>Item id {window.location.href.substr(window.location.href.indexOf('/shop/') + 6)} page</h2>}
