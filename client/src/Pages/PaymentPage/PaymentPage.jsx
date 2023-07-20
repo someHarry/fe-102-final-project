575c16bca07eb7e2fdde29372d928a0047a3ccae
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2023-07-20 16:06:17 +0300

    add

---
+++ client/src/Pages/PaymentPage/PaymentPage.jsx
@@ -0,0 +1,19 @@
+// import './PaymentPage.scss'
+
+// function PaymentPage() {
+//   return (
+//     <>
+//     <section>
+//       <div className="routes">
+//         <p className="routes-title">1. MY BAG</p>
+//         <hr className="routes-line" />
+//         <p className="routes-title">2. DELIVERY</p>
+//         <hr className="routes-line" />
+//         <p className="routes-title">3. REVIEW & PAYMENT</p>
+//       </div>
+//       </section>
+//       </>
+//   )
+// }
+
+// export default PaymentPage
