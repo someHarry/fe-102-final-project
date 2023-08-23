680d1f68d0d2a808fe4d65a0425a40fdc7de654b
KarinaIvaschenko <karinka2323@gmail.com>
2023-08-23 10:52:06 +0300

    redux

--- client/src/redux/reduser.js
+++ client/src/redux/reduser.js
@@ -2,8 +2,6 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import cartReducer from './cart/reduserCart'
 
-const rootReduser = combineReducers({
-  reduser: { cart: cartReducer },
-})
+const rootReduser = combineReducers({ cart: cartReducer })
 
 export default rootReduser
