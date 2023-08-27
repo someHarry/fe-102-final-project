d6d07f83fa1d1f62c1a7d11c472d0a03662f77eb
Hlib Tulupov <glebtulupov2001@gmail.com>
2023-08-27 16:03:58 +0300

    cart component

--- client/src/Pages/CartPage/CartPage.jsx
+++ client/src/Pages/CartPage/CartPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './CartPage.scss'
-import CartComponent from '../../Components/CartComponent/'
+import CartComponent from '../../Components/CartComponent/CartComponent'
 import MayLike from '../../Components/MayLike'
 import RadioButton from '../../Components/RadioButton'
 
