473828d67a949332b604c1bc78c7d946549e55f9
KarinaIvaschenko <karinka2323@gmail.com>
2023-08-29 12:38:02 +0300

    redux

--- client/src/redux/bankCard/reduserBankCard.js
+++ client/src/redux/bankCard/reduserBankCard.js
@@ -1,17 +1,21 @@
 /* eslint-disable no-param-reassign */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createReducer } from '@reduxjs/toolkit'
-import actionAddBankCard from './actionBankCard'
+import { actionAddBankCard, actionClearBankCard } from './actionBankCard'
 
 const initialState = {
   bankCard: '' || JSON.parse(localStorage.getItem('card')),
 }
 
 const reduserBankCard = createReducer(initialState, (builder) => {
-  builder.addCase(actionAddBankCard, (state, { payload }) => {
-    state.bankCard = payload
-    localStorage.setItem('card', JSON.stringify(state.bankCard))
-  })
+  builder
+    .addCase(actionAddBankCard, (state, { payload }) => {
+      state.bankCard = payload
+      localStorage.setItem('card', JSON.stringify(state.bankCard))
+    })
+    .addCase(actionClearBankCard, (state) => {
+      state.bankCard = ''
+    })
 })
 
 export default reduserBankCard
