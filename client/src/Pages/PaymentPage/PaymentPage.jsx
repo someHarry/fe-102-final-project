7304e7c5c241d46f6ae623de0cca193e7fb7a176
KarinaIvaschenko <karinka2323@gmail.com>
2023-08-29 12:47:14 +0300

    redux

--- client/src/Pages/PaymentPage/PaymentPage.jsx
+++ client/src/Pages/PaymentPage/PaymentPage.jsx
@@ -16,7 +16,7 @@ import Form from '../../Components/Form/Form'
 import '../../Components/Form/Form.scss'
 import Input from '../../Components/Input/Input'
 import '../../Components/Input/Input.scss'
-import actionBankCard from '../../redux/bankCard/actionBankCard'
+import { actionAddBankCard } from '../../redux/bankCard/actionBankCard'
 
 function isValidExpirationDate(value) {
   if (!value) return false
@@ -59,7 +59,7 @@ function PaymentPage() {
     initialValues: { ...initialValues },
     validationSchema: validationSchema,
     onSubmit: (values, { resetForm }) => {
-      dispatch(actionBankCard(values))
+      dispatch(actionAddBankCard(values))
       resetForm()
     },
   })
