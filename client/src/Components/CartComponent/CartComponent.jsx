dc94ca271da6f0140a973605c8ae8b33e6188a46
KarinaIvaschenko <karinka2323@gmail.com>
2023-08-29 14:33:13 +0300

    cart

--- client/src/Components/CartComponent/CartComponent.jsx
+++ client/src/Components/CartComponent/CartComponent.jsx
@@ -1,51 +1,57 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import Button from '../Button'
 import './CartComponent.scss'
+import { actionRemoveCart } from '../../redux/cart/actionCart'
 
-export default function CartComponent({ cartStyles, updateSubtotals }) {
-  const [cartItems, setCartItems] = useState([])
-  const [subtotal, setSubtotal] = useState(0)
+export default function CartComponent({
+  cartStyles,
+  // , updateSubtotals
+}) {
+  const cart = useSelector((state) => state.cart.cart)
+  console.log(cart)
 
-  useEffect(() => {
-    const cartItemsFromStorage = localStorage.getItem('cart')
-    if (cartItemsFromStorage) {
-      setCartItems(JSON.parse(cartItemsFromStorage))
-    }
-  }, [])
+  const dispatch = useDispatch()
+  // const [cartItems, setCartItems] = useState([])
+  // const [subtotal, setSubtotal] = useState(0)
 
-  useEffect(() => {
-    const newSubtotal = cartItems.reduce((total, item) => total + (item.currentPrice || 0) * (item.quantity || 1), 0)
-    setSubtotal(newSubtotal)
-    updateSubtotals(newSubtotal.toFixed(2)) // Округлення до двох знаків після коми
-  }, [cartItems, updateSubtotals])
+  // useEffect(() => {
+  //   const cartItemsFromStorage = localStorage.getItem('cart')
+  //   if (cartItemsFromStorage) {
+  //     setCartItems(JSON.parse(cartItemsFromStorage))
+  //   }
+  // }, [])
 
-  const removeFromCart = (itemNo) => {
-    const updatedCart = cartItems.filter((item) => item.itemNo !== itemNo)
-    setCartItems(updatedCart)
-    localStorage.setItem('cart', JSON.stringify(updatedCart))
-    updateSubtotals(subtotal)
+  // useEffect(() => {
+  //   const newSubtotal = cartItems.reduce((total, item) => total + (item.currentPrice || 0) * (item.quantity || 1), 0)
+  //   setSubtotal(newSubtotal)
+  //   updateSubtotals(newSubtotal.toFixed(2)) // Округлення до двох знаків після коми
+  // }, [cartItems, updateSubtotals])
+
+  const removeFromCart = (el) => {
+    dispatch(actionRemoveCart(el))
   }
 
-  const increaseQuantity = (itemNo) => {
-    const updatedCart = cartItems.map((item) =>
-      item.itemNo === itemNo ? { ...item, quantity: (item.quantity || 1) + 1 } : item
-    )
-    setCartItems(updatedCart)
-    localStorage.setItem('cart', JSON.stringify(updatedCart))
-    updateSubtotals(subtotal)
-  }
+  // const increaseQuantity = (itemNo) => {
+  //   const updatedCart = cartItems.map((item) =>
+  //     item.itemNo === itemNo ? { ...item, quantity: (item.quantity || 1) + 1 } : item
+  //   )
+  //   setCartItems(updatedCart)
+  //   localStorage.setItem('cart', JSON.stringify(updatedCart))
+  //   updateSubtotals(subtotal)
+  // }
 
-  const decreaseQuantity = (itemNo) => {
-    const updatedCart = cartItems.map((item) =>
-      item.itemNo === itemNo && item.quantity > 1 ? { ...item, quantity: (item.quantity || 1) - 1 } : item
-    )
-    setCartItems(updatedCart)
-    localStorage.setItem('cart', JSON.stringify(updatedCart))
-    updateSubtotals(subtotal)
-  }
+  // const decreaseQuantity = (itemNo) => {
+  //   const updatedCart = cartItems.map((item) =>
+  //     item.itemNo === itemNo && item.quantity > 1 ? { ...item, quantity: (item.quantity || 1) - 1 } : item
+  //   )
+  //   setCartItems(updatedCart)
+  //   localStorage.setItem('cart', JSON.stringify(updatedCart))
+  //   updateSubtotals(subtotal)
+  // }
 
-  const CartList = cartItems.map((el) => (
+  const CartList = cart.map((el) => (
     <div className={`cart-${cartStyles}__item`} key={el.itemNo}>
       <span className={`cart-${cartStyles}__item-img`}>
         <img src={`${el.imageUrls}`} alt="" />
@@ -54,14 +60,14 @@ export default function CartComponent({ cartStyles, updateSubtotals }) {
         <span className={`cart-${cartStyles}__item-name-quantity`}>
           <p>{el.name}</p>
           <div className={`cart-${cartStyles}__item-price__quantity`}>
-            <Button btnStyles="Quantity" btnClick={() => decreaseQuantity(el.itemNo)} text="-" />
+            {/* <Button btnStyles="Quantity" btnClick={() => decreaseQuantity(el.itemNo)} text="-" /> */}
             <p>{el.quantity || 1}</p>
-            <Button btnStyles="Quantity" btnClick={() => increaseQuantity(el.itemNo)} text="+" />
+            {/* <Button btnStyles="Quantity" btnClick={() => increaseQuantity(el.itemNo)} text="+" /> */}
           </div>
         </span>
 
         <span className={`cart-${cartStyles}__item-price`}>
-          <Button text="Remove" btnStyles="Remove" btnClick={() => removeFromCart(el.itemNo)} />
+          <Button text="Remove" btnStyles="Remove" btnClick={removeFromCart(el)} />
 
           <p>{el.currentPrice}</p>
         </span>
@@ -84,7 +90,7 @@ export default function CartComponent({ cartStyles, updateSubtotals }) {
       {CartList.length > 0 ? (
         <>
           {cartStyles === 'collection' && <h2>My Bag</h2>}
-          {cartStyles === 'collection' && <div className='cart-collection__container'>{CartList}</div>}
+          {cartStyles === 'collection' && <div className="cart-collection__container">{CartList}</div>}
           {cartStyles === 'lis' && <div>{CartList}</div>}
           <span
             style={{
@@ -99,7 +105,7 @@ export default function CartComponent({ cartStyles, updateSubtotals }) {
           {/* <div className='cart-collection__bottom'> */}
           <div className={`cart-${cartStyles}__subtotal`}>
             <span>Subtotal</span>
-            <span>${subtotal.toFixed(2)}</span>
+            {/* <span>${subtotal.toFixed(2)}</span> */}
           </div>
           <span className={`cart-${cartStyles}__link`}>
             {cartStyles === 'lis' && (
@@ -128,5 +134,5 @@ export default function CartComponent({ cartStyles, updateSubtotals }) {
 
 CartComponent.propTypes = {
   cartStyles: PropTypes.string.isRequired,
-  updateSubtotals: PropTypes.func.isRequired,
+  // updateSubtotals: PropTypes.func.isRequired,
 }
