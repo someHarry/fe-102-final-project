06c7ec8d78135152f0d22c23987eb11a8f8d16f9
KarinaIvaschenko <karinka2323@gmail.com>
2023-10-11 19:40:16 +0300

    edits tests

--- client/src/Components/Footer/footer.test.jsx
+++ client/src/Components/Footer/footer.test.jsx
@@ -5,45 +5,53 @@ import Footer from './index'
 
 describe('Footer', () => {
   test('We have Footer?', () => {
+    const title = 'title'
+    const titleTwo = 'titleTwo'
     const { container } = render(
       <MemoryRouter>
-        <Footer />
+        <Footer clientTitle={title} catalogTitle={titleTwo} />
       </MemoryRouter>
     )
     expect(container).toBeInTheDocument()
   })
   test('Render link', () => {
+    const title = 'title'
+    const titleTwo = 'titleTwo'
     render(
       <MemoryRouter>
-        <Footer />
+        <Footer clientTitle={title} catalogTitle={titleTwo} />
       </MemoryRouter>
     )
     expect(screen.getByText('Jura')).toBeInTheDocument()
   })
   test('Render phone', () => {
+    const title = 'title'
+    const titleTwo = 'titleTwo'
     render(
       <MemoryRouter>
-        <Footer />
+        <Footer clientTitle={title} catalogTitle={titleTwo} />
       </MemoryRouter>
     )
     expect(screen.getByText('+1 (234) 567-890')).toBeInTheDocument()
   })
   test('Render with title', () => {
     const title = 'title'
+    const titleTwo = 'titleTwo'
     render(
       <MemoryRouter>
-        <Footer catalogTitle={title} />
+        <Footer catalogTitle={titleTwo} clientTitle={title} />
       </MemoryRouter>
     )
     expect(screen.getByText(title)).toBeInTheDocument()
   })
   test('Render with clientTitle', () => {
     const title = 'title'
+    const titleTwo = 'titleTwo'
     render(
       <MemoryRouter>
-        <Footer clientTitle={title} />
+        <Footer clientTitle={title} catalogTitle={titleTwo} />
       </MemoryRouter>
     )
-    expect(screen.getByText(title)).toBeInTheDocument()
+    expect(screen.getByText(titleTwo)).toBeInTheDocument()
   })
 })
